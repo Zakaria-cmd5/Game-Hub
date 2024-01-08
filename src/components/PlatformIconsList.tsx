6b5514ed86d4488b89790c0f8f057a539ba47ed1
Zakaria Asad <zasad2587@gmail.com>
2024-01-08 15:26:06 +0200

    Refactor: Remove Duplicated interfaces

--- src/components/PlatformIconsList.tsx
+++ src/components/PlatformIconsList.tsx
@@ -6,7 +6,7 @@ import {
   FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
